6d550983d930601f68f475cc1636e72b63e139c5
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2022-05-09 13:52:44 +0530

    third commit

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo docker info
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+cd BackEndUL
+sudo docker-compose build
+sudo docker-compose up
