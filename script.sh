fc623451e33b3e2ce03c71b407645ded19d294e2
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2022-05-09 13:56:10 +0530

    nn

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ sudo usermod -a -G docker ec2-user
 sudo docker info
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 sudo docker-compose build
 sudo docker-compose up
